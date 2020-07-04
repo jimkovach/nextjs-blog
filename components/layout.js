3c1d764c5a5d1acadaeebe20a96c5a9a1dfe081c
Jim Kovach <kovach.j@wenatcheeschools.org>
2020-07-04 09:05:54 -0700

    2nd commit

--- components/layout.js
+++ components/layout.js
@@ -3,7 +3,7 @@ import styles from './layout.module.css'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
-const name = '[Your Name]'
+const name = 'Mr. Kovach'
 export const siteTitle = 'Next.js Sample Website'
 
 export default function Layout({ children, home }) {
